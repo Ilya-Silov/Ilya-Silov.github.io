e0a674be6084b5e66d88be3cbfd387cd0c307ed4
I-S-Inc <iliya.silov@gmail.com>
2021-05-29 22:01:43 +0300

    2

--- js/script.js
+++ js/script.js
@@ -7,25 +7,23 @@ jQuery(window).scroll(function () {
     }
 });
 
-$(document).ready(function() {
-    $('a[href^="#"]').click(function(){
-    var el = $(this).attr('href');
-    $('body').animate({
-    scrollTop: $(el).offset().top}, 2000);
-    return false;
-    });
-    });
 
-   /*  или
-    $(document).ready(function() {
-        $('a[href^="#"]').click(function () { 
-          elementClick = $(this).attr("href");
-          destination = $(elementClick).offset().top;
-          if($.browser.safari){
-            $('body').animate( { scrollTop: destination }, 1100 );
-          }else{
-            $('html').animate( { scrollTop: destination }, 1100 );
-          }
-          return false;
-        });
-      }); */
+$(function(){
+  $('a[href^="#"]').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+    
+    var sc = $(this).attr("href"),
+        dn = $(sc).offset().top;
+    /*
+    * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+    * dn - определяем положение блока на странице
+    */
+    
+    $('html, body').animate({scrollTop: dn}, 500);
+    
+    /*
+    * 1000 скорость перехода в миллисекундах
+    */
+  });
+})
